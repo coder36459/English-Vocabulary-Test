7ca1fd251e8d800ff0b9c5d2f107f90ed00fd252
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-03-27 19:26:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,38 @@
 "use strict";
 document.body.innerHTML = "<div id=\"root\"></div>";
+
+const Test = () => {
+  const Title = () => {return React.createElement("h1", { class: "p-3 mb-3 mt-2 text-uppercase", id: "title" }, "english vocabulary test");};
+
+  return (
+    React.createElement(React.Fragment, null, 
+    React.createElement("div", { class: "row" }, 
+    React.createElement("div", { class: "col-3" }), 
+    React.createElement("div", { class: "col-md-6 text-center" }, 
+    React.createElement(Title, null), 
+    React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "first-nav" }), 
+    React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "second-nav" }), 
+    React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "third-nav" }), 
+    React.createElement("div", { class: "row mt-4 m-3" }, 
+    React.createElement("div", { class: "col-6 text-start", id: "category" }), 
+    React.createElement("div", { class: "col-6 text-end", id: "number" })), 
+
+    React.createElement("h2", { class: "text-bg-info p-4 m-3", id: "definition" }), 
+    React.createElement("p", { class: "text-center mt-4", id: "choose-term" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-one" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-two" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-three" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-four" }), 
+    React.createElement("div", { class: "row mt-4 m-3" }, 
+    React.createElement("div", { class: "col-6 text-start", id: "time-duration" }), 
+    React.createElement("div", { class: "col-6 text-end", id: "your-score" }))), 
+
+
+    React.createElement("div", { class: "col-3" })), 
+
+    React.createElement("div", { class: "m-3 text-center", id: "footer" })));
+
+
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render( React.createElement(Test, null));
