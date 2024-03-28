359598b2188ca2d4677b5eae4218c4e545e9b834
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-03-28 19:54:01 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,26 +2,36 @@
 document.body.innerHTML = "<div id=\"root\"></div>";
 
 const Test = () => {
-  const Title = () => {return React.createElement("h1", { class: "p-3 mb-3 mt-2 text-uppercase", id: "title" }, "english vocabulary test");};
+  const Title = () => {return (
+      React.createElement("h1", { class: "p-3 mb-3 mt-2 text-uppercase", id: "title" }, "english vocabulary test"));
+  };
+  const CatBtn = () => {return (
+      React.createElement("nav", null, 
+      React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize me-2", id: "first-nav" }, "relationships"), 
+      React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize me-2", id: "second-nav" }, "cinema"), 
+      React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "third-nav" }, "random")));
+
+  };
+  const Footer = () => {return (
+      React.createElement("div", { class: "m-3 text-center", id: "footer" }, "Made by ", React.createElement("a", { href: "https://www.linkedin.com/in/maciej-browarski", target: "_blank" }, "Maciej Browarski")));
+  };
 
   return (
-    React.createElement(React.Fragment, null, 
+    React.createElement("main", null, 
     React.createElement("div", { class: "row" }, 
     React.createElement("div", { class: "col-3" }), 
     React.createElement("div", { class: "col-md-6 text-center" }, 
     React.createElement(Title, null), 
-    React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "first-nav" }), 
-    React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "second-nav" }), 
-    React.createElement("button", { type: "button", class: "btn btn-secondary text-capitalize", id: "third-nav" }), 
+    React.createElement(CatBtn, null), 
     React.createElement("div", { class: "row mt-4 m-3" }, 
     React.createElement("div", { class: "col-6 text-start", id: "category" }), 
     React.createElement("div", { class: "col-6 text-end", id: "number" })), 
 
     React.createElement("h2", { class: "text-bg-info p-4 m-3", id: "definition" }), 
     React.createElement("p", { class: "text-center mt-4", id: "choose-term" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-one" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-two" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-three" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-one" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-two" }), 
+    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-three" }), 
     React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-four" }), 
     React.createElement("div", { class: "row mt-4 m-3" }, 
     React.createElement("div", { class: "col-6 text-start", id: "time-duration" }), 
@@ -30,7 +40,7 @@ const Test = () => {
 
     React.createElement("div", { class: "col-3" })), 
 
-    React.createElement("div", { class: "m-3 text-center", id: "footer" })));
+    React.createElement(Footer, null)));
 
 
 };
