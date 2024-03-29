d9b2cdb452725c135b2def44056c902244fd5e4b
Maciej Browarski <140039573+coder36459@users.noreply.github.com>
2024-03-29 19:44:44 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 "use strict";
 document.body.innerHTML = "<div id=\"root\"></div>";
 
+"use strict";
 const Test = () => {
   const Title = () => {return (
       React.createElement("h1", { class: "p-3 mb-3 mt-2 text-uppercase", id: "title" }, "english vocabulary test"));
@@ -24,18 +25,18 @@ const Test = () => {
     React.createElement(Title, null), 
     React.createElement(CatBtn, null), 
     React.createElement("div", { class: "row mt-4 m-3" }, 
-    React.createElement("div", { class: "col-6 text-start", id: "category" }), 
-    React.createElement("div", { class: "col-6 text-end", id: "number" })), 
+    React.createElement("div", { class: "col-6 text-start", id: "category" }, "Category: Cinema"), 
+    React.createElement("div", { class: "col-6 text-end", id: "number" }, "Definition 1 of 20")), 
 
-    React.createElement("h2", { class: "text-bg-info p-4 m-3", id: "definition" }), 
-    React.createElement("p", { class: "text-center mt-4", id: "choose-term" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-one" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-two" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-three" }), 
-    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-four" }), 
+    React.createElement("h2", { class: "text-bg-info p-4 m-3", id: "definition" }, "definition"), 
+    React.createElement("p", { class: "text-center mt-4", id: "choose-term" }, "Choose matching term:"), 
+    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-one" }, "term one"), 
+    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-two" }, "term two"), 
+    React.createElement("button", { type: "button", class: "btn btn-dark me-1", id: "choose-three" }, "term three"), 
+    React.createElement("button", { type: "button", class: "btn btn-dark", id: "choose-four" }, "term four"), 
     React.createElement("div", { class: "row mt-4 m-3" }, 
-    React.createElement("div", { class: "col-6 text-start", id: "time-duration" }), 
-    React.createElement("div", { class: "col-6 text-end", id: "your-score" }))), 
+    React.createElement("div", { class: "col-6 text-start", id: "time-duration" }, "Test duration: 5:23"), 
+    React.createElement("div", { class: "col-6 text-end", id: "your-score" }, "Your score: 20/20 (100%)"))), 
 
 
     React.createElement("div", { class: "col-3" })), 
